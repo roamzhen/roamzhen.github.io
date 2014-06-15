2270fbcdd202fea43423bcb2704fc65289f05642
roamzhen <roamzhen@gmail.com>
2014-06-15 09:19:53 +0800

    version 1.4 mobile ajust

--- js/index.js
+++ js/index.js
@@ -1,59 +1,112 @@
 var angleTnow=0,
   angleMnow=0,
-  angleBnow=0,
-  product_left=0,
-  product_location=0;
+  angleBnow=0;
+
+//productSlider Class
+var productClass = (function(){
+
+  var product_left=0;
+  var product_location=0;
+
+  var cWidth = document.body.clientWidth;
+
+  var product_dev = Math.min(parseInt(cWidth / 270),3);
+
+  var product_number = Math.ceil(GetClass("product-con").length/product_dev);
+  var product_item_width = 270 * product_dev;
+
+  function productSlider(){
+    cWidth = document.body.clientWidth;
+
+    var product = document.getElementById("product");
+    var arrow_left = document.getElementById("arrow_left");
+    var arrow_right = document.getElementById("arrow_right");
+
+    product_dev = Math.min(parseInt(cWidth / 270),3);
+
+    product_number = Math.ceil(GetClass("product-con").length/product_dev);
+    product_item_width = 270 * product_dev;
+
+    product.style.width = product_number*product_item_width +"px";
+
+    arrow_left.onclick = function(){
+      cWidth = document.body.clientWidth;
+
+      product_dev = Math.min(parseInt(cWidth / 270),3);
+
+      product_number = Math.ceil(GetClass("product-con").length/product_dev);
+      product_item_width = 270 * product_dev;
+
+      product.style.width = product_number*product_item_width +"px";
+
+      if(product_location!=0){
+        product_left = product_location;
+        productionAnimate(product,(-product_item_width));
+      }
+    }
+
+    arrow_right.onclick = function(){
+      cWidth = document.body.clientWidth;
+
+      product_dev = Math.min(parseInt(cWidth / 270),3);
+
+      product_number = Math.ceil(GetClass("product-con").length/product_dev);
+      product_item_width = 270 * product_dev;
+
+      product.style.width = product_number*product_item_width +"px";
+
+      if(product_location!=(product_number-1)*product_item_width){
+        product_left = product_location;
+        productionAnimate(product,product_item_width);
+      }
+    }
+
+
+  }
+
+  function productionAnimate(obj,target){
+    clearInterval(obj.timer);
+    
+    product_location = product_left + target;
+
+    obj.timer=setInterval(function(){
+      product_left+=(product_location - product_left)/4;
+      obj.style.left=(-product_left)+"px";
+
+      if(product_left<=1){
+        product_left = 0;
+        product_location = 0;
+        obj.style.left = "0px";
+        clearInterval(obj.timer);
+      }
+      if(product_left>(product_number-1)*product_item_width){
+        product_left = (product_number-1)*product_item_width;
+        product_location = (product_number-1)*product_item_width;
+        obj.style.left = (product_number-1)*product_item_width + "px";
+        clearInterval(obj.timer);
+      }
+
+      if(Math.abs(product_left-product_location)<=1){
+        product_left = product_location;
+        obj.style.left = (-product_left)+"px";
+        clearInterval(obj.timer);
+      }
+
+    },30);
+  }
+
+  return{
+      productSlider:productSlider
+    }
+}())
+//productSlider class end
 
 window.onload=function(){
   scrollEvent();
   faceSlider();
-  productSlider();
+  productClass.productSlider();
 }
 
-function productSlider(){
-  var product = document.getElementById("product");
-  var arrow_left = document.getElementById("arrow_left");
-  var arrow_right = document.getElementById("arrow_right");
-
-  var product_number = parseInt(GetClass("product-item").length/6);
-  var product_item_width = 810;
-
-  product.style.width = (product_number+1)*product_item_width +"px";
-
-  arrow_left.onclick = function(){
-    if(product_location!=0){
-      product_left = product_location;
-      productionAnimate(product,(-product_item_width));
-    }
-  }
-
-  arrow_right.onclick = function(){
-    if(product_location!=product_number*product_item_width){
-      product_left = product_location;
-      productionAnimate(product,product_item_width);
-    }
-  }
-
-
-}
-
-function productionAnimate(obj,target){
-  clearInterval(obj.timer);
-  
-  product_location = product_left + target;
-
-  obj.timer=setInterval(function(){
-    product_left+=(product_location - product_left)/4;
-    obj.style.left=(-product_left)+"px";
-
-    if(Math.abs(product_left-product_location)<=1){
-      product_left = product_location;
-      obj.style.left = (-product_left)+"px";
-      clearInterval(obj.timer);
-    }
-
-  },30);
-}
 
 
 function scrollEvent(){
@@ -62,13 +115,20 @@ function scrollEvent(){
   var slider3 = document.getElementById("slider3");
   var slider4 = document.getElementById("slider4");
 
+  var slider1Height = slider1.clientHeight;
+  var slider2Height = slider2.clientHeight;
+  var slider3Height = slider3.clientHeight;
+  var slider4Height = slider4.clientHeight;
+
   window.onscroll=function(){
     var scrollTop = Math.max(document.documentElement.scrollTop,document.body.scrollTop);
-    
-    slider1.style.backgroundPosition = "0% " + (-scrollTop*0.5) + "px";
-    slider2.style.backgroundPosition = "0% "+ (310 - scrollTop*0.5) + "px";
-    slider3.style.backgroundPosition = "0% "+ (620 - scrollTop*0.5) + "px";
-    slider4.style.backgroundPosition = "0% "+ (930 - scrollTop*0.5) + "px";
+    var downSpeed = 0.2;
+    var downHeight = scrollTop*downSpeed;
+
+    slider1.style.backgroundPosition = "0% " + (-downHeight) + "px";
+    slider2.style.backgroundPosition = "0% "+ (slider1Height*downSpeed - downHeight) + "px";
+    slider3.style.backgroundPosition = "0% "+ ((slider1Height+slider2Height)*downSpeed - downHeight) + "px";
+    slider4.style.backgroundPosition = "0% "+ ((slider1Height+slider2Height+slider3)*downSpeed - downHeight) + "px";
 
   }
 }
