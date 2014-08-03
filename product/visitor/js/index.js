dc6bc699c12752417ffc932cfb1ac8dea9eb4887
roamzhen <roamzhen@gmail.com>
2014-08-03 23:01:13 +0800

    version 2.3

---
+++ product/visitor/js/index.js
@@ -0,0 +1,16 @@
+/* basic class method */
+function hasClass(obj, cls) {
+    return obj.className.match(new RegExp('(\\s|^)' + cls + '(\\s|$)'));
+}
+
+function addClass(obj, cls) {
+    if (!this.hasClass(obj, cls)) obj.className += " " + cls;
+}
+
+function removeClass(obj, cls) {
+    if (hasClass(obj, cls)) {
+        var reg = new RegExp('(\\s|^)' + cls + '(\\s|$)');
+        obj.className = obj.className.replace(reg, '');
+    }
+}
+/* end basic class method */
