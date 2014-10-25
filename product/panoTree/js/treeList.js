84f02d424bdf2a96009d19dc3cf5af1ecfb52ccd
roamzhen <roamzhen@gmail.com>
2014-10-25 23:01:28 +0800

    buildingGuide

---
+++ product/panoTree/js/treeList.js
@@ -0,0 +1,211 @@
+var treeList = [
+	{
+		"name": "人面子",
+		"href": "http://xy.scau.edu.cn/tree/go.asp?id=109",
+		"miniPic":"images/tree/renmainzi.jpg",
+		"pos":{
+			"lng": 113.361025,
+			"lat": 23.162501
+		},
+		"pov":{
+			"heading": 292,
+			"pitch": 9
+		}
+	},
+	{
+		"name": "塞楝",
+		"href": "http://xy.scau.edu.cn/tree/go.asp?id=114",
+		"miniPic":"images/tree/tangjiaoshu.jpg",
+		"pos":{
+			"lng": 113.361096,
+			"lat": 23.162373
+		},
+		"pov":{
+			"heading": 273,
+			"pitch": 4
+		}
+	},
+	{
+		"name": "黄葛榕",
+		"href": "http://xy.scau.edu.cn/tree/go.asp?id=81",
+		"miniPic":"images/tree/huanggerong.jpg",
+		"pos":{
+			"lng": 113.359714,
+			"lat": 23.160472
+		},
+		"pov":{
+			"heading": 15,
+			"pitch": 5
+		}
+	},
+	{
+		"name": "王棕",
+		"href": "javascript:;",
+		"miniPic":"images/tree/wangzhong.jpg",
+		"pos":{
+			"lng": 113.359229,
+			"lat": 23.160924
+		},
+		"pov":{
+			"heading": 86,
+			"pitch": 24
+		}
+	},
+	{
+		"name": "荔枝",
+		"href": "http://xy.scau.edu.cn/tree/go.asp?id=172",
+		"miniPic":"images/tree/lizhi.jpg",
+		"pos":{
+			"lng": 113.359231,
+			"lat": 23.161507
+		},
+		"pov":{
+			"heading": 56,
+			"pitch": 14
+		}
+	},
+	{
+		"name": "黄葛榕2",
+		"href": "http://xy.scau.edu.cn/tree/go.asp?id=81",
+		"miniPic":"images/tree/huanggerong2.jpg",
+		"pos":{
+			"lng": 113.361316,
+			"lat": 23.16035
+		},
+		"pov":{
+			"heading": 174,
+			"pitch": 1
+		}
+	},
+	{
+		"name": "黄葛榕3",
+		"href": "http://xy.scau.edu.cn/tree/go.asp?id=81",
+		"miniPic":"images/tree/huanggerong2.jpg",
+		"pos":{
+			"lng": 113.361046,
+			"lat": 23.160305
+		},
+		"pov":{
+			"heading": 337,
+			"pitch": -6
+		}
+	},
+	{
+		"name": "黄葛榕4",
+		"href": "http://xy.scau.edu.cn/tree/go.asp?id=81",
+		"miniPic":"images/tree/huanggerong2.jpg",
+		"pos":{
+			"lng": 113.360339,
+			"lat": 23.160303
+		},
+		"pov":{
+			"heading": 62,
+			"pitch": 8
+		}
+	},
+	{
+		"name": "黄葛榕5",
+		"href": "http://xy.scau.edu.cn/tree/go.asp?id=81",
+		"miniPic":"images/tree/huanggerong2.jpg",
+		"pos":{
+			"lng": 113.360430,
+			"lat": 23.160289
+		},
+		"pov":{
+			"heading": 200,
+			"pitch": 9
+		}
+	},
+	{
+		"name": "黄葛榕6",
+		"href": "http://xy.scau.edu.cn/tree/go.asp?id=81",
+		"miniPic":"images/tree/huanggerong2.jpg",
+		"pos":{
+			"lng": 113.359890,
+			"lat": 23.160404
+		},
+		"pov":{
+			"heading": 320,
+			"pitch": 5
+		}
+	},
+	{
+		"name": "黄葛榕7",
+		"href": "http://xy.scau.edu.cn/tree/go.asp?id=81",
+		"miniPic":"images/tree/huanggerong2.jpg",
+		"pos":{
+			"lng": 113.359344,
+			"lat": 23.160638
+		},
+		"pov":{
+			"heading": 214,
+			"pitch": 5
+		}
+	},
+	{
+		"name": "洋紫荆",
+		"href": "javascript:;",
+		"miniPic":"images/tree/yangzijing.jpg",
+		"pos":{
+			"lng": 113.359271,
+			"lat": 23.160667
+		},
+		"pov":{
+			"heading": 203,
+			"pitch": 7
+		}
+	},
+	{
+		"name": "塞楝2",
+		"href": "http://xy.scau.edu.cn/tree/go.asp?id=114",
+		"miniPic":"images/tree/tangjiaoshu.jpg",
+		"pos":{
+			"lng": 113.361166,
+			"lat": 23.162498
+		},
+		"pov":{
+			"heading": 323,
+			"pitch": 10
+		}
+	},
+	{
+		"name": "塞楝3",
+		"href": "http://xy.scau.edu.cn/tree/go.asp?id=114",
+		"miniPic":"images/tree/tangjiaoshu.jpg",
+		"pos":{
+			"lng": 113.361166,
+			"lat": 23.162498
+		},
+		"pov":{
+			"heading": 7,
+			"pitch": 11
+		}
+	},
+	{
+		"name": "黄槿",
+		"href": "http://xy.scau.edu.cn/tree/go.asp?id=114",
+		"miniPic":"images/tree/huangjing.jpg",
+		"pos":{
+			"lng": 113.361166,
+			"lat": 23.162498
+		},
+		"pov":{
+			"heading": 44,
+			"pitch": 6
+		}
+	},
+	{
+		"name": "人面子2",
+		"href": "http://xy.scau.edu.cn/tree/go.asp?id=109",
+		"miniPic":"images/tree/renmainzi.jpg",
+		"pos":{
+			"lng": 113.361166,
+			"lat": 23.162498
+		},
+		"pov":{
+			"heading": 122,
+			"pitch": 5
+		}
+	}
+	
+];
