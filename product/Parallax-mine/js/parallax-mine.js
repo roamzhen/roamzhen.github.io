d297234e4bfdb8d07be4426cb585e64898ea429b
roamzhen <roamzhen@gmail.com>
2015-01-02 13:53:09 +0800

    edit speed

--- product/Parallax-mine/js/parallax-mine.js
+++ product/Parallax-mine/js/parallax-mine.js
@@ -66,7 +66,7 @@ MParallex.prototype._bindScroll = function(){
   this.scrollTimer = setInterval(function(){
     if(that.touched)
       scrollCheck.call(that);
-  },20);
+  },40);
   
   document.body.addEventListener("touchmove",function(){
     that.touched = true;
