756446fca05d3e01d9332db2e26d8b9a9a38fcc3
Ye Zhen Peng <roamzhen@gmail.com>
2016-09-08 23:15:15 +0800

    webAr

--- product/webAr/js/webrtc.js
+++ product/webAr/js/webrtc.js
@@ -36,13 +36,8 @@
 
   webrtc.init = function (video, canvas, option) {
     if(webrtc.useable) {
-      var videoSelector = (option) ? (option) : ({video: true});
-      if (window.navigator.appVersion.match(/Chrome\/(.*?) /)) {
-        var chromeVersion = parseInt(window.navigator.appVersion.match(/Chrome\/(\d+)\./)[1], 10);
-        if (chromeVersion < 20) {
-          videoSelector = "video";
-        }
-      };
+      var cameras = [];
+      var actId = null;
 
       // if many sources try to get the environemt-facing camera
       var go = function(video_source_id){
@@ -56,6 +51,27 @@
           video.setAttribute('height', '100%');
           video.style.height = '100%';
           video.style.width = '100%'; 
+
+          console.log(cameras);
+
+          document.querySelector('.btn-change-camare').addEventListener('touchstart', function(e) {
+            if(cameras.length >= 2){
+              var actCamare =  null;
+              for(var i=0; i<cameras.length; i++) {
+                if (cameras[i]['id'] === actId){
+
+                  if(i != camera.length-1) {
+                    actId = cameras[i-1]['id'];
+                    go(actId);
+                  }else {
+                    actId = cameras[i+1]['id'];
+                    go(actId);
+                  }
+
+                }
+              }
+            }
+          });
 
           if (video.mozCaptureStream) {
             video.mozSrcObject = stream;
@@ -75,8 +91,11 @@
           for (var i = 0; i != source_infos.length; ++i) {
             var source_info = source_infos[i];
             if (source_info.kind === 'video') {
-              if (!selected_source || (source_info.facing && source_info.facing == "environment")) {
+              cameras.push(source_info);
+
+              if (source_info.facing && source_info.facing == "environment") {
                 selected_source = source_info.id;
+                actId = source_info.id;
               }
             }
           }
