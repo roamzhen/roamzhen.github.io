f84a28a651e5369f79d468ca0be6fdea65fa83c7
Ye Zhen Peng <roamzhen@gmail.com>
2016-09-14 15:25:16 +0800

    web ar

--- product/webAr/js/index.js
+++ product/webAr/js/index.js
@@ -61,8 +61,8 @@
   var overlay = document.getElementById('overlay');
   var overlayCC = overlay.getContext('2d');
 
-  vid.width = screenWidth/3;
-  vid.height = screenHeight/3;
+  vid.width = screenWidth/4;
+  vid.height = screenHeight/4;
   overlay.width = screenWidth;
   overlay.height = screenHeight;
 
