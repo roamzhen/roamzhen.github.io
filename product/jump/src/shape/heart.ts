b370d4063918ba60fc4ee6cc5b67276c4d755230
zhenpeng.yzp <zhenpeng.yzp@alibaba-inc.com>
2018-01-27 14:12:59 +0800

    提交跳一跳

---
+++ product/jump/src/shape/heart.ts
@@ -0,0 +1,23 @@
+/**
+ * 心形矢量图
+ */
+class Heart extends egret.Shape{
+    public constructor(){
+        super();
+        this.drawGrid();
+    }
+    private drawGrid(){
+        this.graphics.beginFill( 0x0000ff );
+        this.graphics.drawRect( 0, 0, 50,50 );
+        this.graphics.endFill();
+        this.graphics.beginFill( 0x0000ff );
+        this.graphics.drawRect( 50, 50, 50, 50);
+        this.graphics.endFill();
+        this.graphics.beginFill( 0xff0000 );
+        this.graphics.drawRect( 50, 0, 50,50 );
+        this.graphics.endFill();
+        this.graphics.beginFill( 0xff0000 );
+        this.graphics.drawRect( 0, 50, 50,50 );
+        this.graphics.endFill();               
+    }
+}
