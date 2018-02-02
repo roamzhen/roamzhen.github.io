5f6a8a0481d1987c713c6b763dc7f9139605016a
zhenpeng.yzp <zhenpeng.yzp@alibaba-inc.com>
2018-02-02 17:17:09 +0800

    123

--- product/jump/src/Main.ts
+++ product/jump/src/Main.ts
@@ -147,8 +147,7 @@ class Main extends eui.UILayer {
      * Create scene interface
      */
     protected startCreateMainStage(): void {
-        document.querySelector('.boxLoading').remove();
-
+        // document.querySelector('.boxLoading').remove();
 
         let bg = this.createBitmapByName("bgs_jpg");
         this.addChild(bg);
